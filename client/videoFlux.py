7b4b2005a052ddd97c503e8b00a953d8b801421c
atxme <christophe.benedetti15@gmail.com>
2024-08-06 11:11:07 +0200

    update frame capture and webRtc management

--- client/videoFlux.py
+++ client/videoFlux.py
@@ -3,35 +3,61 @@ import numpy as np
 import os
 import sys 
 from threading import Thread
+from queue import Queue
 
 
 class VideoFlux:
 
-    def initCamera(self):
-        for i in range(10):
-            self.ret, self.frame = self.cap.read()
-            if self.ret:
-                self.capIdx = i
-                break
-            else:
-                None
-
-        if (self.cap.isOpened != True):
-            print("Error: Camera not found")
-            sys.exit(0)
-
-    def getFrame(self):
-        
-
-
-
     def __init__(self) -> None:
-        self.cap = cv.VideoCapture(0)
         self.capIdx = 0
-        self.cap.set(3, 640)
-        self.cap.set(4, 480)
-        self.cap.set(10, 100)
         self.frame = None
         self.ret = None
         self.isOpened = None
         self.frame = None
+        self.queue = Queue()
+
+    def initCamera(self):
+        for i in range(10):
+            self.cap = cv.VideoCapture(i)
+            self.isOpened = self.cap.isOpened()
+            if self.isOpened:
+                print(f"Camera found at index {i}")
+                self.capIdx = i
+                break
+            else:
+                i+=1
+        
+        if (self.cap.isOpened() != True):
+            print("Error: Camera not found")
+            sys.exit(0)
+        
+        self.cap.set(cv.CAP_PROP_FRAME_WIDTH, 640)
+        self.cap.set(cv.CAP_PROP_FRAME_HEIGHT, 480)
+        self.cap.set(cv.CAP_PROP_FPS, 30)
+
+        
+    def captureFrame(self):
+        while True:
+            self.ret, self.frame = self.cap.read()
+            self.queue.put(self.frame)
+            if cv.waitKey(1) & 0xFF == ord('q'):
+                break
+        self.releaseCamera()
+
+    def releaseCamera(self):
+        self.cap.release()
+        cv.destroyAllWindows()
+
+    
+    def run(self):
+        self.initCamera()
+        Thread(target=self.captureFrame).start()
+
+
+
+
+        
+
+
+
+    
--- client/webRtcManagement.py
+++ client/webRtcManagement.py
@@ -1,4 +1,50 @@
-from aiortc import RTCPeerConnection, RTCSessionDescription
-from aiortc.contrib.media import MediaPlayer, MediaBlackhole
-    
+import asyncio
+import json
+import cv2
+from aiortc import RTCPeerConnection, RTCSessionDescription, VideoStreamTrack
+from aiortc.contrib.media import MediaPlayer
+from aiortc.mediastreams import VideoFrame
+import aiohttp
+from threading import Thread
+from videoFlux import VideoFlux
 
+class VideoFrameTrack(VideoStreamTrack):
+    def __init__(self, videoFlux : VideoFlux):
+        super().__init__()
+        self.videoFlux = videoFlux
+
+    async def recv(self):
+        frame = self.videoFlux.queue.get()
+        pts, time_base = await self.next_timestamp()
+        return VideoFrame.from_ndarray(frame, format="bgr24").rebuild(pts=pts, time_base=time_base)
+
+class WebRTCClient:
+    def __init__(self, server_url: str, videoFlux):
+        self.server_url = server_url
+        self.pc = RTCPeerConnection()
+        self.video_flux = videoFlux
+        self.video_track = VideoFrameTrack(videoFlux)
+
+    async def login_to_host(self):
+        async with aiohttp.ClientSession() as session:
+            offer = await self.pc.createOffer()
+            await self.pc.setLocalDescription(offer)
+
+            async with session.post(f"{self.server_url}/offer", json={
+                "sdp": self.pc.localDescription.sdp,
+                "type": self.pc.localDescription.type,
+            }) as response:
+                answer = await response.json()
+
+            await self.pc.setRemoteDescription(RTCSessionDescription(
+                sdp=answer["sdp"], type=answer["type"]
+            ))
+
+            # Keep the connection open
+            while True:
+                await asyncio.sleep(1)
+
+    def start(self):
+        # Add the video track to the peer connection
+        self.pc.addTrack(self.video_track)
+        asyncio.run(self.login_to_host())
